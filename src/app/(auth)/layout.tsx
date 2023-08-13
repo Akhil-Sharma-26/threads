548bb91aa9631717343f1013cd2d40ae012d4ef0
Akhil-Sharma-26 <asharma1_be22@thapar.edu>
2023-08-13 23:51:26 +0530

    top-bar-added

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,11 +2,7 @@ import {ClerkProvider} from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 import "../globals.css"
 // for SEO(Search Engine Optamization): 
-export const metadata={
-    title:"camp-aurora",
-    description:"Hello World!",
 
-}
 const inter= Inter({subsets: ["latin"]}); // To import fonts into nextJS
 
 export default function RootLayout({children}:{children:React.ReactNode}) { // type of the prop has been specified here, ie The prop is a object of type ReactNode
