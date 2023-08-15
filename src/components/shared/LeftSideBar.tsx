0922ab8a6b9841f32ddd43b6247c2c76c1013aa4
Akhil-Sharma-26 <asharma1_be22@thapar.edu>
2023-08-15 19:18:13 +0530

    Home-page-strucutred

--- src/components/shared/LeftSideBar.tsx
+++ src/components/shared/LeftSideBar.tsx
@@ -1,5 +1,43 @@
+"use client"
+import {sidebarLinks} from "@/constants"
+import { SignOutButton, SignedIn } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import {usePathname,useRouter} from 'next/navigation'
 export default function LeftSideBar(){
+    const router=useRouter();
+    const pathname=usePathname();
     return(
-        <section className=""></section>
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link)=>{
+                    const linkIsActive=(pathname.includes(link.route) && link.route.length > 1) || pathname===link.route;
+                    return(
+                        <Link href={link.route}
+                        key={link.label}
+                        className={`leftsidebar_link ${linkIsActive && 'bg-primary-500'}`}>
+                        <Image src={link.imgURL}
+                        alt={link.label}
+                        width={24}
+                        height={24}/>
+                        <p className="text-light-1 max-lg:hidden">
+                            {link.label}
+                        </p>
+                        </Link>
+                    
+                )})}
+            </div>
+            <div className="mt-10 px-6">
+            <SignedIn>
+                        {/* this code is only going to be shown if we are signed in */}
+                        <SignOutButton signOutCallback={()=>router.push('/sign-in')}>
+                            <div className="flex cursor-pointer gap-4 p-4">
+                                <Image src="/assets/logout.svg" alt="logout" width={24} height={24}></Image>
+                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+            </div>
+        </section>
     )
 }
